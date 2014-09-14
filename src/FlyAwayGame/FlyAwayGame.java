7c175466215566c753a04de56a9bb56b5ee66124
Chayenjr <Chayenjr@genuine.microsoft.com>
2014-09-14 18:18:50 +0700

    jump Limit

--- src/FlyAwayGame/FlyAwayGame.java
+++ src/FlyAwayGame/FlyAwayGame.java
@@ -1,7 +1,5 @@
 package FlyAwayGame;
 
-import java.awt.im.InputContext;
-
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
@@ -9,7 +7,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.util.InputAdapter;
 
 public class FlyAwayGame extends BasicGame{
 
@@ -18,11 +15,12 @@ public class FlyAwayGame extends BasicGame{
 	public static final int vx = -5;
 	public static int x = 0;
 	public static final float FLYDOT_JUMP_VY = 5;
-	public static final float G = (float) -0.2;
+	public static float G = (float) -0.2;
 	private FlyDot flydot;
 	public static boolean isStarted;
 	private int score = 0;
 	private AngleBow anglebow;
+	private int jumpLimit = 6;
 	
 	public FlyAwayGame(String title) {
 		super(title);
@@ -38,6 +36,13 @@ public class FlyAwayGame extends BasicGame{
 		if (isStarted) {
 			anglebow.remove();
 		}
+		jumpPic();
+	}
+
+
+	public void jumpPic() throws SlickException {
+		Image jumppic = new Image("C:///Users/Chayenjr/Desktop/junior/KU Ле 2/OOP/Fly Away/jump"+jumpLimit+".png");
+		jumppic.draw(20,40);
 	}
 
 
@@ -60,7 +65,7 @@ public class FlyAwayGame extends BasicGame{
 		if(isStarted) {
 			angleDistanceBeforeStarted();
 			x += vx;
-			score += -vx;
+			scoreSummary();
 		}
 		if(flydot.isCollide()) {
 			isStarted = false;
@@ -69,6 +74,14 @@ public class FlyAwayGame extends BasicGame{
 			container.reinit();
 			score = 0;
 			x = 0;
+		}
+	}
+
+
+	public void scoreSummary() {
+		score += -vx;
+		if (score % 1500 == 0) {
+			jumpLimit++;
 		}
 	}
 
@@ -93,7 +106,7 @@ public class FlyAwayGame extends BasicGame{
 			isStarted = true;
 		}
 	    if (key == Input.KEY_SPACE) {
-	    	flydot.jump();
+	    	jumpLimit();
 	    }
 	    if (isStarted == false && key == Input.KEY_UP) {
 	    	anglebow.increaseAngle();
@@ -103,6 +116,16 @@ public class FlyAwayGame extends BasicGame{
 	    	anglebow.decreaseAngle();
 	    	anglebow.update();
 	    }
+	}
+	
+	public void jumpLimit() {
+		if (jumpLimit > 0) {
+			flydot.jump();
+			jumpLimit --;
+		}
+		if (jumpLimit > 6) {
+			jumpLimit = 6;
+		}
 	}
 
 	public static void main(String[] args) {

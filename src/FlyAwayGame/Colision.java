20a6ee97cf969213fcfc27458cc5f69f107115ff
Chayenjr <Chayenjr@158.108.138.45>
2014-10-27 14:59:59 +0700

    add to git

--- src/FlyAwayGame/Colision.java
+++ src/FlyAwayGame/Colision.java
@@ -25,11 +25,11 @@ public class Colision {
 	}
 	
 	static boolean isCollideItemHeart(float x, float y) {
-		if ((FlyDot.x+30 <= x+60 && FlyDot.x+30 >= x) && (FlyDot.y+30 <= 600-y+60 && FlyDot.y+30 >= 600-y)) {
+		if ((FlyDot.x+30 <= x+60 && FlyDot.x+30 >= x) && (FlyDot.y+30 <= 600-y+80 && FlyDot.y+30 >= 600-y)) {
 			FlyAwayGame.heart += 1;
 			if (FlyAwayGame.heart >= 3) {
 				FlyAwayGame.heart = 3;
-				FlyAwayGame.score += 800;
+				FlyAwayGame.jumpLimit++;
 			}
 			return true;
 		}
